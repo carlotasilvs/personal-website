5d53a41038e7ee6f72194c052b48b6f6a261909b
Carlota Silva <carlotasilva@MacBook-Air-de-Carlota-2.local>
2022-01-06 17:43:25 +0000

    created sticky effect on profile image and added projects carousel

--- js/accordion.js
+++ js/accordion.js
@@ -3,13 +3,16 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
     var infoContainer = this.nextElementSibling;
 
     if (infoContainer.style.display === "block") {
         infoContainer.style.display = "none";
+        this.classList.toggle("closed");
+        this.classList.remove("active");
     } else {
         infoContainer.style.display = "block";
+        this.classList.toggle("active");
+        this.classList.remove("closed");
     }
   });
 }
