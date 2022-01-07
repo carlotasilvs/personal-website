fdc659532f1677b74dda21e1efca84958a696171
Carlota Silva <carlotasilva@AirdeCarlota2.lan>
2022-01-07 18:53:01 +0000

    finished responsiveness

--- js/accordion.js
+++ js/accordion.js
@@ -1,5 +1,6 @@
 var acc = document.getElementsByClassName("open");
 var i;
+var profile = document.getElementById("profileImgMobile");
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
@@ -9,10 +10,12 @@ for (i = 0; i < acc.length; i++) {
         infoContainer.style.display = "none";
         this.classList.toggle("closed");
         this.classList.remove("active");
+        profile.style.opacity = 1;
     } else {
         infoContainer.style.display = "block";
         this.classList.toggle("active");
         this.classList.remove("closed");
+        profile.style.opacity = 0;
     }
   });
 }
